7e4c1969ef97f052ae91fc5284a6d235aea98462
Bala-10xacademy <bala10xacademy@gmail.com>
2023-05-10 06:09:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [timerId, setTimerId] = useState(null);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (time > 0) {
+      intervalId = setInterval(() => {
+        setTime((prevTime) => prevTime - 1);
+      }, 1000);
+
+      setTimerId(intervalId);
+    } else {
+      clearInterval(timerId);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [time]);
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      const inputValue = parseInt(event.target.value);
+
+      if (isNaN(inputValue) || inputValue <= 0) {
+        setTime(0);
+      } else {
+        setTime(Math.floor(inputValue));
+      }
+
+      event.target.value = "";
+    }
+  };
+
+  const handleReset = () => {
+    clearInterval(timerId);
+    setTimerId(null);
+    setTime(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input type="text" id="timeCount" onKeyDown={handleKeyDown} />
+      <div id="current-time" className={time === 0 ? "expired" : ""}>
+        {time}
+      </div>
+      {time === 0 && timerId !== null && <button onClick={handleReset}>Reset</button>}
     </div>
   );
 }
